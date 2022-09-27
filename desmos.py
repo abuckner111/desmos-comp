ca4cd63f6a27e538304b6be4af0edd4daf1209e9
abuckner111 <abuckner111@gmail.com>
2022-09-27 17:21:35 -0400

    Added runtime rules

--- desmos.py
+++ desmos.py
@@ -32,30 +32,84 @@ dnum = -1
 dpoi = -2
 
 arch = [ 16,2, dnum,0, dnum,0, dpoi,5, dpoi,0, dpoi,0, dpoi,0, dpoi,0, dpoi,0, dpoi,0, dpoi,0, dpoi,0, dpoi,0, dnum,0 ]
-prg = [igoto,16]
+prgp = len(arch)/2+1
+print ( "prgp: "+str(prgp))
+prg = [igoto,prgp]
 varp = len(arch)+len(prg)
 vars = [-1,69]
 D = arch
 D.extend(prg)
 D.extend(vars)
-
-
+D[0] = prgp
 
 def change(pos, datax, datay):
-	D[2*pos-1] = datax
-	D[2*pos]   = datay
+	D[2*pos-2] = datax
+	D[2*pos-1] = datay
+
+def next():
+	change(1, D[0]+1, 2)
 
 def tick():
-	ins = D[D[0]*2-1]
+	ins = D[int(D[0]*2-2)]
 	cnt = D[1]
-	dat = D[D[0]*2]
+	dat = D[int(D[0]*2-1)]
 	if ins == igoto:
 		if cnt >= 2:
 			change(1,dat,2)
+		else:
+			next()
 	if ins == iifeq:
 		if cnt >= 2:
 			if D[3] == D[5]:
 				change(14, dnum, 1)
 			else:
 				change(14, dnum, 0)
-		change(1, D[0]+1, 2)
+		else:
+			next()
+	if ins == iifgt:
+		if cnt >= 2:
+			if D[3] > D[5]:
+				change(14, dnum, 1)
+			else:
+				change(14, dnum, 0)
+		else:
+			next()
+	if ins == iiflt:
+		if cnt >= 2:
+			if D[3] < D[5]:
+				change(14, dnum, 1)
+			else:
+				change(14, dnum, 0)
+		else:
+			next()
+	if ins == iifge:
+		if cnt >= 2:
+			if D[3] >= D[5]:
+				change(14, dnum, 1)
+			else:
+				change(14, dnum, 0)
+		else:
+			next()
+	if ins == iifle:
+		if cnt >= 2:
+			if D[3] <= D[5]:
+				change(14, dnum, 1)
+			else:
+				change(14, dnum, 0)
+		else:
+			next()
+
+	if D[1] >= 2:
+		change(1, D[0], D[1]+1)
+	if D[1] == 1:
+		change(1, D[0]+1, D[1])
+	if D[1] <= 0:
+		return -1
+	return 0
+
+
+
+
+while (tick() >= 0):
+	print (str(D[0])+", "+str(D[1]), end='\r')
+print ()
